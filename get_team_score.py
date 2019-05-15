12e5a3084c8ae39a0f776e33f910667f414dbc33
fuverdred <fred.cook@bristol.ac.uk>
2019-05-15 16:24:39 +0100

    Minor changes to output format

--- get_team_score.py
+++ get_team_score.py
@@ -1,7 +1,4 @@
 import requests
-import pickle
-from collections import defaultdict
-from itertools import combinations
 from unidecode import unidecode
 
 from bs4 import BeautifulSoup as bs
@@ -39,7 +36,7 @@ example_team = ('Primoz Roglic',
 team = [rider for rider in riders if unidecode(rider.name) in example_team]
 
 for rider in team:
-    print('{:20}{:4d}'.format(rider.name, rider.points))
-print('total score: {:d}'.format(sum([r.points for r in team])))
+    print('{:20}{:5d}'.format(rider.name, rider.points))
+print('{}\n{:25d}'.format('='*25,sum([r.points for r in team])))
 
 
