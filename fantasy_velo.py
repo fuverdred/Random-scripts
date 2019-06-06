6924f089b76812a2a5cdb027cfa1adeb2b31a7dd
fuverdred <fred.cook@bristol.ac.uk>
2019-06-06 17:45:28 +0100

    Minor changes to simplify fantasy_velo picker

--- fantasy_velo.py
+++ fantasy_velo.py
@@ -5,46 +5,41 @@ from itertools import combinations
 
 from bs4 import BeautifulSoup as bs
 
+TEAM_SIZE = 9
+
+EXCEPTIONS = {'Chris Froome': 'Christopher Froome',
+              'Felix Großschartner': 'Felix Grossschartner',
+              'Magnus Cort': '.php?id=132428',
+              'Alexey Lutsenko': '.php?id=138294',
+              'Mickaël Cherel': '.php?id=140603',
+              'Paweł Bernas': '.php?id=134451',
+              'Carl Fredrik Hagen': '.php?id=179795',
+              'Steve Cummings': '.php?id=140584'}
+
 
 class Rider(object):
-    def __init__(self, name, price, style):
+    def __init__(self, name, price):
         self.name = name
         self.price = price
-        self.style = style
         self.points = 0
 
     def __repr__(self):
-        return str((self.name, self.style))
-
-class Team(object):
-    def __init__(self):
-        self.team = {"All Rounder": [],
-                     "Climber": [],
-                     "Sprinter": [],
-                     "Unclassed": [],
-                     "Wildcard": []}
-        self.price = 0
-        self.score =0
-
-    def __repr__(self):
-        return '\n'.join([r.name for _, lst in self.team.items() for r in lst])
+        return str((self.name, self.points))
 
 
 def scrape_info():
     ###Scrape the riders from velogames################################
-    p = requests.get('https://www.velogames.com/italy/2019/riders.php')
+    p = requests.get('https://www.velogames.com/dauphine/2019/riders.php')
     soup = bs(p.content, 'html.parser')
     table = soup.find('tbody')
     riders = [Rider(list(row.findAll('td'))[1].text,
-                    int(list(row.findAll('td'))[-1].text),
-                    list(row.findAll('td'))[3].text)
+                    int(list(row.findAll('td'))[-1].text))
               for row in table.findAll('tr')]
     ###################################################################
 
     ###Scrape the points from Procyclingstats##########################
-    def get_points(name, year=''):
-        pcs_base = 'https://www.procyclingstats.com/rider/'
-        p = requests.get(pcs_base + '-'.join(name.split()) + '/' + str(year))
+    def get_points(url):
+        p = requests.get(url)
         soup = bs(p.content, 'html.parser')
         results = soup.find(class_='results')
         if results is None:
@@ -52,86 +47,56 @@ def scrape_info():
         score = int(results.text.split()[-1])
         return score
 
+    pcs_base = 'https://www.procyclingstats.com/rider/'
     for rider in riders:
-        print(rider.name)
-        score = get_points(rider.name)
-        if score is not None:
-            rider.points = score
+        if rider.name in EXCEPTIONS.keys():
+            if 'php' in EXCEPTIONS[rider.name]:
+                url = pcs_base[:-1] + EXCEPTIONS[rider.name]
+            else:
+                url = pcs_base + '-'.join(EXCEPTIONS[rider.name].split())
+        else:
+            url = pcs_base + '-'.join(rider.name.split())
+        print(url)
+        rider.points = get_points(url)
     ###################################################################
     return riders
 
-#riders = scrape_info()
-with open('giro_riders.pickle', 'rb') as f:
-    riders = pickle.load(f)
-riders = [r for r in riders if hasattr(r, 'points')] #  Some scrapage didnt work
+riders = scrape_info()
 
-styles = defaultdict(list)
-for rider in riders:
-    styles[rider.style].append(rider)
+by_price = defaultdict(list)
 
-prices = defaultdict(list)
-for rider in riders:
-    prices[rider.price].append(rider)
+for rider in riders: #  Group riders by price
+    by_price[rider.price].append(rider)
 
-for key, lst in styles.items():
-    lst.sort(key=lambda x: x.points, reverse = True)
-for key, lst in prices.items():
-    lst.sort(key=lambda x: x.points, reverse = True)
+for key in by_price.keys(): # Sort each price by how many points they have
+    by_price[key].sort(key=lambda x:x.points, reverse=True)
 
+possibles = []
 
+for price in by_price.keys():
+    if price > 10:
+        possibles += by_price[price]
+    else:
+        possibles += by_price[price][:3] #  Only a few of the chaff
+
+best_team = []
 best_score = 0
-best_team = None
 
-team = Team()
+print('\n\nThis may take a while, cancel when '
+      'you get bored for best team so far...\n\n')
 
-
-for all_round in combinations(styles["All Rounder"], 2):
-    team.team["All Rounder"] = all_round
-    team.score += sum([r.score for r in all_round])
-    team.price += sum([r.price for r in all_round])
-    for climber in combinations(styles["Climber"], 2):
-        for sprinter in styles["Sprinter"]:
-            for unclassed in combinations(styles["Unclassed"], 3):
-                team = list(all_round + climber + unclassed) + [sprinter]
-                price = sum([r.price for r in team])
-                if price > 100:
-                    continue
-                for wildcard in [r for r in styles["Wildcard"]
-                                 if r.price <= 100 - price]:
-                    team += [wildcard]
-                    score = sum([r.score for r in team])
-                    if score > best_score:
-                        best_score = score
-                        best_team = team[:]
-                    
-                
-
-    
-    
-
-def recursive(riders, team=[], price=0, score=0, depth=0):
-    depth += 1
-    #print(' '*depth, depth)
-
-    global best_score
-    global best_team
-
-    if score > best_score and len(team)==9:
+for count, team in enumerate(combinations(possibles, TEAM_SIZE)):
+    total_price = sum([rider.price for rider in team])
+    if total_price > 100:
+        continue #  Too expensive
+    score = sum([rider.points for rider in team])
+    if score > best_score:
         best_score = score
-        best_team = team
+        best_team = team[:]
 
-    if depth > 9 or 100-price > (100-depth)*4:
-        return
-
-    for index, rider in enumerate(riders, 1):
-        if price + rider[1] > 100:
-            continue
-
-        team.append(rider)
-        recursive(riders[index:], team, price+rider[1], score+rider[2], depth)
-        team.remove(rider)
-
-
-#recursive(reduced)
-
-
+for rider in best_team:
+    print('{:20}|{:4d}|{:6d}'.format(rider.name, rider.price, rider.points))
+print('{:20}|{:4d}|{:6d}'.format('',
+                                 sum([r.price for r in best_team]),
+                                 sum([r.points for r in best_team])))
+    
